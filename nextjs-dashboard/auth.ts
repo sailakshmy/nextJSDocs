5ecb14841b9380cbf69f71c2460863990b85c9b4
Sai Lakshmy <sailakshmy94@gmail.com>
2024-01-28 22:20:26 +0400

    Commit-13:- Chapter-15:- Adding Authentication

---
+++ nextjs-dashboard/auth.ts
@@ -0,0 +1,31 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import Credentials from 'next-auth/providers/credentials';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+import { getUser } from './app/lib/data';
+
+ 
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [Credentials({
+    async authorize(credentials) {
+      const parsedCredentials = z
+        .object({ email: z.string().email(), password: z.string().min(6) })
+        .safeParse(credentials);
+        if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user = await getUser(email);
+            if (!user) return null;
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+            if (passwordsMatch) return user;
+          }
+          console.log('Invalid credentials');
+        return null;
+    },
+  }),],
+  secret: process.env.AUTH_SECRET
+});
